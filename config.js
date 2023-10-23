f1d59c9f330a7fe8c73f7d3df721dc514661e40e
gv-sh <gv-sh@outlook.com>
2023-10-23 12:14:48 +0530

    rebase

--- config.js
+++ config.js
@@ -1,124 +1,94 @@
-import * as THREE from 'three';
-
 export const config = {
-    fog: {
-        color: 0x000000,
-        near: 10,
-        far: 30,
-        visible: false
-    },
-    onScreenControls: {
-        consoleTimeout: 3000,
-        fpsStats: true,
-    },
-    showGridHelper: false,
-    followCamOffset: new THREE.Vector3(0, 1.5, -1.5),
-    bokehPass: false,
-    renderer: {
-        antialias: true,
-        shadowMapEnabled: true,
-        shadowMapType: THREE.PCFSoftShadowMap,
-        toneMapping: THREE.ACESFilmicToneMapping,
-    },
-    orbitControls: true,
-    avatar: {
-        animations: {
-            idle: 1,
-            turnLeft: 2,
-            turnRight: 3,
-            turnBack: 4,
-            walking: 5
+    global: {
+        showStats: true,
+        camera: {
+            fov: 100,
+            near: 0.1,
+            far: 15,
+            initialPosition: [0, 1.35, -1],
         },
-        skeletonHelper: false,
-        speed: 0.04,
-        turnSpeed: 0.01,
-        visible: true,
         orbitControls: {
-            minDistance: 2,
+            enabled: true,
+            minDistance: 1,
             maxDistance: 2,
             minPolarAngle: Math.PI / 4,
-            maxPolarAngle: Math.PI / 2
+            maxPolarAngle: Math.PI / 2,
+            damping: {
+                enabled: true,
+                factor: 0.05,
+            },
+            screenSpacePanning: false,
         },
-        modelPath: 'gltf/Walter3.glb',
-        walkingSound: 'mp3/step_soundwav-14903.mp3',
-        walkingSoundVolume: 0.5
+        dracoPath: 'draco/',
+        player: {
+            speed: 1.2
+        }
     },
-    camera: {
-        fov: 80,
-        near: 0.35,
-        far: 20,
-        lerpFactor: 0.2, // Determines how quickly the camera follows: lower is slower
-        lookAtLerpFactor: 0.5, // Determines how quickly the camera looks at the target: lower is slower
-    },
-    scenes: [
+    levels: [
         {
             name: 'MethLab',
-            modelPath: 'gltf/MethLAB.glb',
-            environmentMap: 'hdr/environment.hdr',
-            elevationOffset: -.25,
-            walkableRegion: {
-                vertices: [
-                    new THREE.Vector3(-0.8, 0, -0.6),
-                    new THREE.Vector3(0.6, 0, -0.6),
-                    new THREE.Vector3(0.6, 0, 8),
-                    new THREE.Vector3(-0.8, 0, 8),
-                    new THREE.Vector3(-0.8, 0, -0.6)
-                ],
-                visible: false,
-                color: 0x00ff00,
+            scene: {
+                modelPath: 'glb/MethLAB.glb',
+                environmentMap: 'hdr/environment.hdr',
+                elevationOffset: -.25,
+                bgm: 'mp3/deep-research-144798.mp3',
+                walkableRegions: {
+                    whitelist: [
+                        [
+                            [-0.8, 0, -0.6],
+                            [0.6, 0, -0.6],
+                            [0.6, 0, 8],
+                            [-0.8, 0, 8],
+                            [-0.8, 0, -0.6] // Last vertex must be same as first vertex
+                        ],
+                        [
+                            // Add vertices of next polygon here
+                        ]
+                    ],
+                    visible: true,
+                    color: 0x00ff00,
+                },
+                interactiveRegions: {
+                    whitelist: [
+                        [
+                            [-0.5, 0, 2.5],
+                            [1, 0, 2.5],
+                            [1, 0, 4.5],
+                            [-0.5, 0, 4.5],
+                            [-0.5, 0, 2.5] // Last vertex must be same as first vertex
+                        ],
+                        [
+                            // Add vertices of next polygon here
+                        ]
+                    ],
+                    visible: true,
+                    color: 0xff0000,
+                },
+                teleportRegions: {
+                    whitelist: [
+                        [
+                            [-0.5, 0, 5.5],
+                            [1, 0, 5.5],
+                            [1, 0, 7.5],
+                            [-0.5, 0, 7.5],
+                            [-0.5, 0, 5.5] // Last vertex must be same as first vertex
+                        ],
+                        [
+                            // Add vertices of next polygon here
+                        ]
+                    ],
+                    visible: true,
+                    color: 0x0000ff,
+                }
             },
-            interactiveRegion: {
-                vertices: [
-                    new THREE.Vector3(-0.5, 0, 2.5),
-                    new THREE.Vector3(1, 0, 2.5),
-                    new THREE.Vector3(1, 0, 4.5),
-                    new THREE.Vector3(-0.5, 0, 4.5),
-                    new THREE.Vector3(-0.5, 0, 2.5)
-                ],
-                visible: false,
-                color: 0xff0000,
-            },
-            bgm: 'mp3/deep-research-144798.mp3',
-            bgmVolume: 0.01
-        },
-        {
-            name: 'Diner',
-            modelPath: 'gltf/diner.glb',
-            environmentMap: 'hdr/Cracked Road HDR.hdr',
-            elevationOffset: 0,
-            walkableRegion: {
-                vertices: [
-                    new THREE.Vector3(-4.5, 0, -1),
-                    new THREE.Vector3(6.2, 0, -1),
-                    new THREE.Vector3(6.2, 0, 1.5),
-                    new THREE.Vector3(-4.5, 0, 1.5),
-                    new THREE.Vector3(-4.5, 0, -1)
-                ],
-                visible: false,
-                color: 0x00ff00,
-            },
-            interactiveRegion: {
-                vertices: [
-                    new THREE.Vector3(5, 0, -1),
-                    new THREE.Vector3(7, 0, -1),
-                    new THREE.Vector3(7, 0, 1),
-                    new THREE.Vector3(5, 0, 1),
-                    new THREE.Vector3(5, 0, -1)
-                ],
-                visible: false,
-                color: 0xff0000,
-            },
-            bgm: 'mp3/fun-times-together-112809.mp3',
-            bgmVolume: 0.01
+            player: {
+                modelPath: 'glb/Walter3.glb',
+                initialPosition: [0, 0, 0],
+
+                initialOrientation: Math.PI,
+                scale: 1,
+                walkSound: 'mp3/step_soundwav-14903.mp3',
+            }
         }
-    ],
-    postProcessing: {
-        enabled: false,
-        bloomParams: {
-            threshold: 0,
-            strength: 0.2,
-            radius: 0,
-            exposure: 1,
-        }
-    }
+    ]
 };
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,152 @@
-import { useEffect, useRef } from 'react';
-import Head from 'next/head';
-import styles from '../styles/styles.module.css';
+import * as THREE from 'three';
+import { useEffect, useState } from 'react';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
 import { config } from '../config.js';
+import { Level } from '../lib/Level.js';
 
-export default function Home() { 
+
+const CubePage = () => {
+
+    const [loadingProgress, setLoadingProgress] = useState(0);
+    const [loadingText, setLoadingText] = useState('Loading assets...');
+    const [isLoaded, setIsLoaded] = useState(false);
+    let soundOn = true; // Assuming sound is on by default
+
+
+    useEffect(() => {
+
+        let level = new Level(0);
+
+        if (module.hot) {
+            module.hot.dispose(() => {
+                // Stop all sounds 
+                level.bgm.stop();
+            });
+        }
+
+        const soundButton = document.getElementById('soundToggle');
+
+        soundButton.addEventListener('click', () => {
+            soundOn = !soundOn;
+
+            if (soundOn) {
+                soundButton.innerText = "Sound: ON";
+                // Add a class soundOn to the body to indicate that sound is on
+                soundButton.classList.add('soundOn');
+                level.setVolume(0.5);
+                level.keyboardController.setVolume(0.5);
+
+            } else {
+                soundButton.innerText = "Sound: OFF";
+                // Remove the class soundOn from the body to indicate that sound is off
+                soundButton.classList.remove('soundOn');
+                level.setVolume(0);
+                level.keyboardController.setVolume(0);
+            }
+        });
+
+        const main = async () => {
+
+            setLoadingText('Initialising renderer...');
+            await level.initialiseRenderer(window);
+            setLoadingProgress(prev => prev + 5);
+
+            setLoadingText('Loading environment map...');
+            await level.loadEnvMap();
+            setLoadingProgress(prev => prev + 10);
+
+            setLoadingText('Loading scene...');
+            await level.loadScene();
+            setLoadingProgress(prev => prev + 50);
+
+            setLoadingText('Setting up camera...');
+            await level.setupCamera();
+            setLoadingProgress(prev => prev + 5);
+
+            setLoadingText('Setting up orbit controls...');
+            await level.setupOrbitControls(level.camera);
+            setLoadingProgress(prev => prev + 5);
+
+            setLoadingText('Loading player...');
+            await level.loadPlayer();
+            setLoadingProgress(prev => prev + 15);
+
+            setLoadingText('Starting...');
+            level.addGridHelper();
+
+            level.bindKeyboardEvents();
+
+            level.bgm.play();
+
+            document.getElementById('cube-container').appendChild(level.renderer.domElement);
+
+            level.animate();
+
+            level.bindStats(new Stats());
+            config.global.showStats ? document.body.appendChild(level.stats.dom) : null;
+
+            setLoadingProgress(prev => prev + 5);
+
+            // After 300ms, set isLoaded to true to remove the loading screen
+            setTimeout(() => {
+                setIsLoaded(true);
+            }, 500);
+        }
+
+        main();
+
+    }, []);
+
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>MethLAB Experience</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main className={styles.main}>
-                <h1 className={styles.title}>MethLAB Experience</h1>
-                <ul className={styles.list}>
-                    <li><a className={styles.link} href="/scene1">{ config.scenes[0].name } (Work in Progress)</a></li>
-                    <li><a className={styles.link} href="/scene2">{ config.scenes[1].name } (Work in Progress)</a></li>
-                    <li><a className={styles.link} href="/scene3">Scene 3 (Unavailable)</a></li>
-                </ul>
-            </main>
+        <div>
+            {!isLoaded && (
+                <div id="overlay">
+                    <div id="loading">
+                        <div id="loading-bar">
+                            <div id="loading-progress" style={{ width: `${loadingProgress}%` }}></div>
+                        </div>
+                        <div id="loading-text">{loadingText}</div>
+                    </div>
+                </div>
+            )}
+            <div id="cube-container" style={{ width: '100vw', height: '100vh' }}>
+            </div>
+            <div id="left-bottom-ui">
+                <div className="table">
+                    <div className="row">
+                        <div className="cell">W</div>
+                        <div className="cell">Forward</div>
+                    </div>
+                    <div className="row">
+                        <div className="cell">A</div>
+                        <div className="cell">Left</div>
+                    </div>
+                    <div className="row">
+                        <div className="cell">S</div>
+                        <div className="cell">Backward</div>
+                    </div>
+                    <div className="row">
+                        <div className="cell">D</div>
+                        <div className="cell">Right</div>
+                    </div>
+                    <div className="row">
+                        <div className="cell">Mouse</div>
+                        <div className="cell">Look around</div>
+                    </div>
+                    <div className="row">
+                        <div className="cell">SPACE</div>
+                        <div className="cell">Interact</div>
+                    </div>
+                </div>
+            </div>
+            <div id="bottom-ui">
+            </div>
+            <div id="right-bottom-ui">
+                <button id="soundToggle">{soundOn ? "Sound: ON" : "Sound: OFF"}</button>
+            </div>
         </div>
     );
-}
+};
+
+export default CubePage;
